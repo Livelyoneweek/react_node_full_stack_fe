78d060a391f50a4ea830e6ff6b92761dea48f755
Livelyoneweek <wlvvwhs@naver.com>
2023-11-25 20:41:34 +0900

    feat: react-router-dom add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import NavBar from './layout/Navbar'
+import Footer from './layout/Footer'
+
+
+function Layout() {
+  return (
+    <div>
+      <NavBar />
+        <main>
+          <Outlet />
+        </main>
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <>
-      <div className='App'>
-        <h1 className='text-3xl font-bold underline'>
-          Hello World!
-        </h1>
-      
-      </div>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<LandingPage />} />
+
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+        </Route>
+      </Routes>
     </>
   )
 }
