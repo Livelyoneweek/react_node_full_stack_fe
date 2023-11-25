012fb834d470485231584354445aa5e9aa0275fe
Livelyoneweek <wlvvwhs@naver.com>
2023-11-26 01:14:17 +0900

    feat: redux persist add

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from './userSlice';
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
+
+
+// persist 설정
+const rootReducer = combineReducers({
+    user: userReducer,
+})
+
+// persist 설정
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+// persist 설정
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer
-    }
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: {
+            ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    })
 })
+
+export const persistor   = persistStore(store)
